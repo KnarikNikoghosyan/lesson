7bd01121362e8b2100d9b140a2530369af74bbad
KnarikNikoghosyan <knarik.n@mail.ru>
2021-10-20 20:40:37 +0400

    python basic

---
+++ lesson2.py
@@ -0,0 +1,41 @@
+# letter = input("write a letter ")
+# l1 = letter.upper() == letter
+# print(l1)
+
+# age = int(input("your age "))
+# result = age>0
+# print(result)
+
+
+# age = int(input("user age "))
+# result = 18<age<25
+# print(result)
+
+# print("Hello " * 15)
+
+
+# day = int(input("the day "))
+# second = day*24*60*60
+# print(second)
+
+# c = float(input("celsius "))
+# f = 32 + c * 1.8
+# print(f)
+
+# f = float(input("farenheit "))
+# c = ( f - 32 )/ 1.8
+# print(c)
+
+# angle1 = float(input("angle1 "))
+# angle2 = float(input("angle2 "))
+# angle3 = 180 - angle1 - angle2
+# print(angle3)
+# result = 90 == angle3
+# print(result)
+
+
+# number = int(input("number "))
+# even = number // 2
+# odd = number - even
+# odd2 = even + number % 2
+# print(f"even {even} Odd {odd}")
