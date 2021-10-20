0c1a6f7e04a03a5c982cb443879be93de7cc8ae5
KnarikNikoghosyan <knarik.n@mail.ru>
2021-10-20 20:42:28 +0400

    python basic

---
+++ lesson4.py
@@ -0,0 +1,76 @@
+# x = 5
+# y = 5
+# print(x is y)
+# print(x == y)
+# print(id(x),id(y))
+
+# x = 5
+# y = x
+# # y += 2
+# # x += 2
+# x = 2
+# print(x,y)
+# print(x is y)
+# print(x == y)
+# print(id(x),id(y))
+
+# name = "Ani"
+# res = "n" in name
+# print(res)
+
+# age = int(input("age "))
+# age_result = 18<age<30 
+# work_exp = int(input("working experience "))
+# work_exp_result = work_exp > 2
+# education = input("education ").lower()
+# education_result = "high" == education
+# result = age_result and work_exp_result and education_result
+# print(result)
+
+
+# price = int(input("price "))
+# perc = int(input("percentage "))
+# price += price*perc / 100
+# print(price)
+
+# apple = input("have apple in your fridge ").lower() == "y"
+# banana = input("have cherry in your fridge ").lower() == "y"
+# cherry = input("have cherry in your fridge ").lower() == "y"
+# light = input("have you light at home? ").lower() == "y"
+# res_apple = apple and not light
+# res_banana = banana and not light
+# res_cherry = cherry and not light
+# print("remove apple from fridge", res_apple)
+# print("remove banana from fridge ", res_banana)
+# print("remove cherry from fridge", res_cherry)
+
+
+# x = 1234
+# first = x % 10
+# second = x % 100 // 10
+# tirth = x % 1000 // 100
+# forth = x // 1000 
+# print(first,second,tirth,forth)
+
+# number = int(input("number "))
+# res_number = 99 < number < 1000
+# print(res_number)
+
+
+
+# nerk = 56000
+# ator = 27
+# result = nerk // ator
+# print(result)
+# avelcuk = nerk - result*ator
+# # or
+# avelcuk1 = nerk % ator
+# print(avelcuk)
+# print(avelcuk1)
+
+
+# notebooks = "dell hp hp asus "
+# res = "hp hp " in notebooks
+# c = notebooks.count("hp")
+# print(res)
+# print(c)
