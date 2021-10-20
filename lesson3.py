e07e1f8f35638c561af88b533a4cffc41f3ab433
KnarikNikoghosyan <knarik.n@mail.ru>
2021-10-20 20:41:54 +0400

    python basic

---
+++ lesson3.py
@@ -0,0 +1,99 @@
+import string
+import math
+from math import *
+import random
+import random as r
+import time
+import os
+import calendar
+import datetime
+
+# letters = string.ascii_letters
+# lower = string.ascii_lowercase
+# upper = string.ascii_uppercase
+# numbers = string.digits
+# chars = string.punctuation
+# print(letters)
+# print(lower)
+# print(upper)
+# print(numbers)
+# print(chars)
+
+# print(math.pi)
+# print(math.sqrt(16))
+# print(math.factorial(5))
+
+# print(pi)
+# print(e)
+# print(sqrt(16))
+
+# print(math.ceil(1.2))  
+# print(math.floor(0.4))  
+# print(math.pow(2, 3))  
+# print(round(1.486, 2))
+# print(format(432.4567, ".2f"))
+
+# print(random.randint(0,10))
+
+# letters = "abcdefgh"
+# print(random.choice(letters))
+
+# letters = string.ascii_letters
+# password = ""
+# l = random.choice(letters)
+# password += l
+# l = random.choice(letters)
+# password += l
+# l = random.choice(letters)
+# password += l
+# l = random.choice(letters)
+# password += l
+# l = random.choice(letters)
+# password += l
+# l = random.choice(letters)
+# password += l
+# print(password)
+
+# print(r.randrange(0,100,5))
+
+# print(r.randrange(1,100, 2))
+
+# print("Hello Siri ")
+# time.sleep(1)
+# print("Hi")
+
+# print("path",os.getcwd())
+
+# path = f'{os.getcwd()}/con'
+# os.mkdir(path) 
+
+# y = int(input("Input the year : "))
+# m = int(input("Input the month : "))
+# print(calendar.month(y, m))
+
+# x = datetime.datetime.now()
+# print(x)
+# print(datetime.date.today())
+
+
+# x = datetime.datetime.now()
+# born = int(input("born "))
+# print(x.year - born)
+# print(x.year)
+# print(x.strftime("%A"))
+
+# dembeli cragri hashvum ,ays orvanov
+# x = datetime.datetime.now()
+# y = int(input("year ")) + 2
+# m = int(input("month "))
+# d = int(input("day "))
+# left = (datetime.datetime(y, m, d) - x)
+# print(left)
+
+# dembeli cragri hashvum
+# x = datetime.datetime.now()
+# y = int(input("year "))
+# m = int(input("month "))
+# d = int(input("day "))
+# left = (x - datetime.datetime(y, m ,d))
+# print(left)
