236a483441a1dddbc92239c3a5c46e3e08041bb2
KnarikNikoghosyan <knarik.n@mail.ru>
2021-10-20 20:34:10 +0400

    python basic

---
+++ lesson1.py
@@ -0,0 +1,58 @@
+# print("Hello world")
+"""problem"""
+# age = 27
+# print(type(age))
+# print(id(age))
+
+# x = 5.6
+# print(id(x))
+# print(type(x))
+
+# age = int(input("How old are you? ")) 
+# print(2021-age)
+
+# print("Hello" + "7")
+# print("5" + "4")
+# print(5 + 4)
+
+# print(int("5") + 4)
+
+# name = input("What is your name? ")
+# age = int(input("How old are you? "))
+# print("Hello " + name)
+# print("Hello" + " " + name)
+# print("Hello", name)
+# print("Hello.My name is " + name + " I am " + str(age) + " years old")
+# print("Hello.My name is", name , "I am" , age, "years old")
+
+# print(f"Hello my name is {name} I am {age} years old")
+
+# number = float(input("input number "))
+
+# print("Hello world ", end="\n")
+# print("okay ")
+
+
+# print("\tHello \nworld")
+# 2age = 5
+# print(2age)
+
+# x y = 12
+# x-y = 12
+# x,y = 5,7
+# print(x,y)
+
+# x = True
+# y = False
+# print(type(x),type(y))
+
+
+# print(True + True)
+# print(True / False)
+
+answer = "Windows"
+answer = answer.upper()
+print(answer)
+
+answer = answer.lower()
+print(answer)
